5cf77d4537cdc23c81f0abc795a21e7eb18cb775
Your Name <you@example.com>
2023-03-07 10:55:25 +0000

    Bug Fix - make action icons on movie card configurable.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -9,7 +9,6 @@ import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
-import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
 import img from '../../images/film-poster-placeholder.png'
 import Avatar from '@mui/material/Avatar';
@@ -18,8 +17,8 @@ import { MoviesContext } from "../../contexts/moviesContext";
 
 
 
-export default function MovieCard({movie}) {
-  const { favorites, addToFavorites } = useContext(MoviesContext);
+export default function MovieCard({ movie, action }) {
+  const { favorites } = useContext(MoviesContext);
 
   if (favorites.find((id) => id === movie.id)) {
     movie.favorite = true;
@@ -27,10 +26,7 @@ export default function MovieCard({movie}) {
     movie.favorite = false
   }
 
-  const handleAddToFavorite = (e) => {
-    e.preventDefault();
-    addToFavorites(movie);
-  };
+  
 
 
   return (
@@ -75,16 +71,12 @@ export default function MovieCard({movie}) {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-      <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
-
-    <Link to={`/movies/${movie.id}`}>
+        {action(movie)}
+        <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
           </Button>
         </Link>
-
       </CardActions>
     </Card>
   );
